6693a48921ca4c3519d223ada359c7f1121f161c
Djomar Lopulissa <djomarmaluku@gmail.com>
2024-04-17 19:27:44 +0200

    Dark Mode toegevoegd Text shadow voor input vak

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 const inputvak = document.getElementById("inputvak");
+const checkLightOfDark = document.getElementById("checkLightOfDark");
+var thema = "";
+
 let NetUitGerekent = false;
 
 function voegAanDisplay(input) {
@@ -15,7 +18,7 @@ function voegAanDisplay(input) {
 
 function clearDisplay() {
   inputvak.value = "";
-  localStorage.clear();
+  localStorage.removeItem("opgeslagenInputValue");
 }
 
 function uitrekenen() {
@@ -27,12 +30,41 @@ function uitrekenen() {
     alert(error);
     inputvak.value = "Error";
     NetUitGerekent = true;
-    localStorage.clear();
+    localStorage.removeItem("opgeslagenInputValue");
   }
 }
 
+// Wanneer de pagina opnieuw word geladen of word geladen.
 window.onload = function () {
   if (localStorage.getItem("opgeslagenInputValue") != null) {
     inputvak.value = localStorage.getItem("opgeslagenInputValue");
   }
+  
+  if (localStorage.getItem("thema") == "dark") {
+    document.getElementById("body").style.backgroundColor = "black";
+    document.getElementById("darkmodeText").style.color = "white";
+    document.getElementById("checkLightOfDark").checked = true;
+  }
+  else {
+    document.getElementById("body").style.backgroundColor = "white";
+    document.getElementById("darkmodeText").style.color = "black";
+    document.getElementById("checkLightOfDark").checked = false;
+  }
+}
+
+function checkboxChanged(v) {
+    if (v.checked == true) {
+      console.log(v.checked);
+      document.getElementById("body").style.backgroundColor = "black";
+      document.getElementById("darkmodeText").style.color = "white";
+      thema = "dark";
+      localStorage.setItem("thema", thema)
+    }
+    else {
+      console.log(v.checked);
+      document.getElementById("body").style.backgroundColor = "white";
+      document.getElementById("darkmodeText").style.color = "black";
+      thema = "light";
+      localStorage.setItem("thema", thema)
+    }
 }
