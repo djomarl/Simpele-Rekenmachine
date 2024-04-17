035077ec4831bf5c8150527ee16fe52665a0b683
Djomar Lopulissa <djomarmaluku@gmail.com>
2024-04-17 18:04:15 +0200

    Waardes worden nu opgeslagen in localStorage.

--- main.js
+++ main.js
@@ -5,22 +5,34 @@ function voegAanDisplay(input) {
   if (NetUitGerekent == true) {
     inputvak.value = "";
     inputvak.value += input;
+    localStorage.setItem("opgeslagenInputValue", inputvak.value);
     NetUitGerekent = false;
   } else {
     inputvak.value += input;
+    localStorage.setItem("opgeslagenInputValue", inputvak.value);
   }
 }
 
 function clearDisplay() {
   inputvak.value = "";
+  localStorage.clear();
 }
 
 function uitrekenen() {
   try {
     inputvak.value = "Ant: " + eval(inputvak.value);
+    localStorage.setItem("opgeslagenInputValue", inputvak.value);
     NetUitGerekent = true;
   } catch (error) {
+    alert(error);
     inputvak.value = "Error";
     NetUitGerekent = true;
+    localStorage.clear();
+  }
+}
+
+window.onload = function () {
+  if (localStorage.getItem("opgeslagenInputValue") != null) {
+    inputvak.value = localStorage.getItem("opgeslagenInputValue");
   }
 }
